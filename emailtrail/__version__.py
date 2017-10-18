5f0746f2b51bc918193ff412326e588f9905ca47
Akshay Kumar <akshay.kmr4321@gmail.com>
2017-10-19 02:32:01 +0530

    pypi packaging

--- emailtrail/__version__.py
+++ emailtrail/__version__.py
@@ -1,9 +1,9 @@
 """ versioning """
 __title__ = 'emailtrail'
-__description__ = """Analyze email headers for structured information,
-                     calculate delay from hops in transit"""
-__version__ = '0.1.2'
+__description__ = 'Analyse hops taken by an Email to reach you. Get structured information about each hop - Hostnames, Protocol used, Timestamp, and Delay'
+__version__ = '0.3.14'
 __author__ = 'Akshay Kumar'
 __author_email__ = 'akshay.kmr4321@gmail.com'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 Akshay Kumar'
+__url__ = 'https://github.com/akshayKMR/emailtrail'
